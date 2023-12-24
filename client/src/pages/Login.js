149d21902d63999760ae01b5cc1a4c4ae79be816
Mark Steven Alba <marksteven.alba@gmail.com>
2023-12-25 07:55:08 +0800

    login banner

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -70,15 +70,14 @@ const SignInSide = () => {
                     sm={4}
                     md={7}
                     sx={{
-                        // backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
-                        // backgroundRepeat: 'no-repeat',
+                        backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+                        backgroundRepeat: 'no-repeat',
                         backgroundColor: (t) =>
                             t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
                         backgroundSize: 'cover',
                         backgroundPosition: 'center',
                     }}
                 >
-                <CoinSpin/>
                 </Grid>
                 <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
                     <Box
