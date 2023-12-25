43428fc18c0460589d0e62f40e6716057ed0f17a
Mark Steven Alba <marksteven.alba@gmail.com>
2023-12-25 08:04:15 +0800

    Fix api

--- client/src/utils/http-common.js
+++ client/src/utils/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://sharewin.pro/api",
+    baseURL: "https://sharewin.pro",
     headers: {
         "Content-type": "application/json",
     },
