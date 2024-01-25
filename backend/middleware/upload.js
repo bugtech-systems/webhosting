3ee03bec00bb519d3ffe98cdfd5379dc244dc1b4
Mark Steven Alba <marksteven.alba@gmail.com>
2024-01-25 10:22:13 +0800

    Fix create database api

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 
-const storagePath = process.env.STORAGE_PATH;
+const storagePath = process.env.STORAGE_PATH + '/storageFolder';
 
 let storage = multer.diskStorage({
   destination: function (req, file, cb) {
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-
+mongoose.set('strictQuery', true);
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -28,11 +28,19 @@ mongoose.connect(process.env.PRIMARY_STRING, {
     const fileRoutes = require('./routes/fileRoutes');
     const deploymentRoutes = require('./routes/deploymentRoutes');
     const userRoutes = require('./routes/userRoutes');  
+    const { executeScript } = require('./utils/pgsqlHelper');
 
 
 app.get('/', (req, res) => {
     res.send('TESTING API!')
 })
+
+// Create a route to trigger the script execution
+app.get('/create-user-database', (req, res) => {
+  executeScript();
+  res.send('Script execution initiated');
+});
+
 
 app.use('/apiv2/auth', authRoutes);
 app.use('/apiv2/file', fileRoutes);
